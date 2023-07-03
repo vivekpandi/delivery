a8f84c5efd8c32220dd3f4197593d2f206c9efc4
vivek <viviek@teghasoft.com>
2023-07-03 16:46:46 +0530

    added changes

---
+++ src/component/ScreenComponents/MyAccountScreenComponent/about-us.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const AboutusComponent = () => {
+  return (
+    <View>
+      <Text>index1</Text>
+    </View>
+  );
+};
+
+export default AboutusComponent;
